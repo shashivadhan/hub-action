1f7145dfc8a6810827c35852209a9370e972f46a
shashivadhan <50444325+shashivadhan@users.noreply.github.com>
2025-04-27 11:30:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello from Cloud Run!'
+def hello_world():
+    return 'Hello, World!'
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+    # Ensuring the app runs on the correct port
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
