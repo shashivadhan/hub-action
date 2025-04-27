b153bb663cf4f35b26dbd48c172d42cbd369a609
shashivadhan <50444325+shashivadhan@users.noreply.github.com>
2025-04-27 12:32:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello Ultimetrix!'
 
 if __name__ == '__main__':
     # Ensuring the app runs on the correct port
